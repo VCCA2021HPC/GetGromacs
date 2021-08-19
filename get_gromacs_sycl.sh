123df28a5d0d007f0999837d865c1e2de1631cb8
Benson Muite <bkmgit@users.noreply.github.com>
2021-08-19 22:59:07 +0300

    Add multinode build

--- get_gromacs_sycl.sh
+++ get_gromacs_sycl.sh
@@ -9,25 +9,24 @@ source /opt/intel/inteloneapi/setvars.sh --force
 # Make directory for the project
 mkdir GROMACS-SYCL
 cd GROMACS-SYCL
-# Get a recent version of cmake
-wget https://github.com/Kitware/CMake/releases/download/v3.21.1/cmake-3.21.1.tar.gz
-tar -xvf cmake-3.21.1.tar.gz 
-cd cmake-3.21.1/
-./bootstrap --prefix=${HOME}/GROMACS-SYCL/cmake-3.21.1-install
-make
-make install
-cd ..
 
 # Get Gromacs
 wget https://ftp.gromacs.org/gromacs/gromacs-2021-sycl.tar.gz
 tar -xf gromacs-2021-sycl.tar.gz
 cd gromacs-2021-sycl
-mkdir build
-cd build
 echo "#!/bin/bash" > build.sh
+echo "mkdir build-single-node" >> build.sh
+echo "cd build-single-node" >> build.sh
 echo "cmake  -DGMX_BUILD_OWN_FFTW=ON -DREGRESSIONTEST_DOWNLOAD=ON -DGMX_GPU=SYCL -DCMAKE_C_COMPILER=icx -DCMAKE_CXX_COMPILER=icpx -DCMAKE_INSTALL_PREFIX=${HOME}/GROMACS-SYCL/gromacs-2021-sycl-install .." >> build.sh
 echo "make" >> build.sh
 echo "make install" >> build.sh
+echo "cd .." >> build.sh
+echo "mkdir build-multi-node" >> build.sh
+echo "cd build-multi-node" >> build.sh
+echo "cmake -DGMX_BUILD_OWN_FFTW=ON -DREGRESSIONTEST_DOWNLOAD=ON -DGMX_GPU=SYCL -DCMAKE_C_COMPILER=icx -DCMAKE_CXX_COMPILER=icpx -DCMAKE_INSTALL_PREFIX=${HOME}/GROMACS-SYCL/gromacs-2021-sycl-install2 -DGMX_MPI=ON -DBUILD_SHARED_LIBS=off  -DGMXAPI=OFF .." >> build.sh
+echo "make -j2" >> build.sh
+echo "make install" >> build.sh
+echo "cd .." >> build.sh
 qsub -l nodes=1:gen9:ppn=2 -d . build.sh
 
 
