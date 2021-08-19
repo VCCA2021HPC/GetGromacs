a7a0c1b20efc7edb8f7a7f1b890dea688924fa02
Benson Muite <bkmgit@users.noreply.github.com>
2021-08-19 20:08:37 +0300

    Improve build
    Follows https://community.intel.com/t5/Intel-oneAPI-Data-Parallel-C/Building-GROMACS-SYCL/m-p/1282147

--- get_gromacs_sycl.sh
+++ get_gromacs_sycl.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Get and install SYCL version of GROMCS
+# based on https://community.intel.com/t5/Intel-oneAPI-Data-Parallel-C/Building-GROMACS-SYCL/m-p/1282147
 
 cd $HOME
 # Set variables
@@ -23,10 +24,11 @@ tar -xf gromacs-2021-sycl.tar.gz
 cd gromacs-2021-sycl
 mkdir build
 cd build
-$HOME/GROMACS-SYCL/cmake-3.21.1-install/bin/cmake .. -DGMX_BUILD_OWN_FFTW=ON -DREGRESSIONTEST_DOWNLOAD=ON \
- -DGMX_MPI=on -DCMAKE_INSTALL_PREFIX=${HOME}/Gromacs-SYCL/gromacs-2021-sycl-install
-make
-make install
+echo "#!/bin/bash" > build.sh
+echo "cmake  -DGMX_BUILD_OWN_FFTW=ON -DREGRESSIONTEST_DOWNLOAD=ON -DGMX_GPU=SYCL -DCMAKE_C_COMPILER=icx -DCMAKE_CXX_COMPILER=icpx -DCMAKE_INSTALL_PREFIX=${HOME}/GROMACS-SYCL/gromacs-2021-sycl-install .." >> build.sh
+echo "make" >> build.sh
+echo "make install" >> build.sh
+qsub -l nodes=1:gen9:ppn=2 -d . build.sh
 
 
 
